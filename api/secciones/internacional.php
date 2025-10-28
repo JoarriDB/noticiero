2910f423d1ebe148ffeb0ee257821972c2ff8673
Joa-db <joarridb@gmail.com>
2025-10-28 17:07:25 -0500

    Imágenes

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -7,5 +7,6 @@ $internacional = [
 "titulo" => "Cumbre climática acuerda reducción de emisiones para 2035",
 "autor" => "María Pérez",
 "resumen" => "Líderes internacionales pactaron objetivos comunes para acelerar la descarbonización y aumentar la financiación para países en desarrollo.",
+"imagen" => "/images/internacional/cumbre.jpg",
 ];
 ?>
