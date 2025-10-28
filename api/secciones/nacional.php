9a83e39ca92611d0ffb07a5338b10dddb7fc9469
Joa-db <joarridb@gmail.com>
2025-10-28 17:37:19 -0500

    imagen nacional

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -7,5 +7,6 @@ $nacional = [
 "titulo" => "Gobierno anuncia inversión en infraestructuras regionales",
 "autor" => "Carolina Ruiz",
 "resumen" => "El plan contempla mejoras en carreteras y servicios públicos para reducir la brecha entre zonas urbanas y rurales.",
+"imagen" => "/images/nacional/nacional.jpg",
 ];
 ?>
